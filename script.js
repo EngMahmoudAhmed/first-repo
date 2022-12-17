5825e627a21fabd5cff68a48398182c536613eb1
Mahmoud-Ahmed <lya440676@gmail.com>
2022-12-17 20:14:02 +0200

    changed the name of html file

---
+++ script.js
@@ -0,0 +1,8 @@
+const index = document.getElementById('index')
+const action = document.getElementById('action')
+
+
+function isAction() {
+    index.classList.toggle('is-active')
+    action.classList.toggle('is-active')
+}
